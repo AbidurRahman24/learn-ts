b6d7ef35dd753ea2f81407c9d2e59b370a631288
Abidur Niloy <niloyabidur@gmail.com>
2024-11-02 12:47:21 +0600

    typeALias

---
+++ module1/src/typeAlias.ts
@@ -0,0 +1,14 @@
+{
+// type alias is a way to create a custom name for a specific type.
+    type user = {
+        name: string;
+        age: number;
+        contactNum?: number;
+        gender: string;
+    }
+    let newUser:user={
+        name: 'Niloy',
+        age: 28,
+        gender: 'male'
+    }
+}
