751e095ffb0ebbb91763f7a784990e75186fdb26
Abidur Niloy <niloyabidur@gmail.com>
2024-11-02 11:32:19 +0600

    object

---
+++ module1/src/Object.ts
@@ -0,0 +1,19 @@
+
+{
+
+const user: {
+    readonly company:string;
+    firstName:string;
+    middleName?: string;
+    lastName: string;
+    Age: number;
+    merried: boolean;
+} = {
+    company: 'brine stand 23',
+    firstName: 'Niloy',
+    lastName: 'Mahmud',
+    Age: 26,
+    merried: true,
+};
+
+}
---
+++ module1/src/basisicDataType.ts
@@ -0,0 +1,15 @@
+// String
+let message: string = "Hello, new ts learner";
+
+// number 
+let age: number = 26;
+
+// boolean
+let isStudent: boolean = true;
+
+// array
+let numbers:number[] = [1,2,3,4];
+let friend:string[] = ["Niloy", "Maya"];
+
+// tuple
+let user:[string,number] = ['niloy',26];
