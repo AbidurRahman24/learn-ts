3115892c67536c4182193ce61901772450d0a37e
Abidur Niloy <niloyabidur@gmail.com>
2024-11-02 11:41:17 +0600

    function with ts

---
+++ module1/src/function.ts
@@ -0,0 +1,18 @@
+// function
+
+function great(name:string):string{
+    return `Hello, ${name}`
+}
+
+// Default Parameters
+
+function great2(name:string = 'niloy'):string{
+    return `Hello, ${name}`;
+
+}
+
+// Void Return Type: Use void for functions that don’t return anything.
+function logMessage(message: string): void {
+    console.log(message);
+  }
+  
